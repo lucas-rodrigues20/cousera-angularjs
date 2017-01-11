167e12b4eea0cace5c503e1da3389c2d6631704e
Lucas Rodrigues <lucas.rodrigues20@outlook.com>
2017-01-11 10:32:51 -0200

    assignment 03 setup

---
+++ assignment03/app.js
@@ -0,0 +1,11 @@
+(function(){
+	'use strict';
+
+	angular.module('NarrowItDownApp', [])
+		.controller('NarrowItDownController', NarrowItDownController);
+
+	function NarrowItDownController(){
+
+	};
+
+})();
